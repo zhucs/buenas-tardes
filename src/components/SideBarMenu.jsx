42db284318890a45407f69b959afac7f1b228cb6
Kent Liu <kentliukentliu@gmail.com>
2023-06-11 13:27:45 -0400

    basic-lesson-page sidebar open close

--- src/components/SideBarMenu.jsx
+++ src/components/SideBarMenu.jsx
@@ -1,16 +1,22 @@
 import '../App.css'
 
 import {sidebarItems} from "../constants/index.js";
-import {useContext} from "react";
+import {useContext, useState} from "react";
 import {LanguageContext} from "../constants/LanguageContext.jsx";
 
 const SideBarMenu = () => {
     const { language } = useContext(LanguageContext);
+    const[open, setOpen] = useState(true);
 
     return (
         <div className="flex">
-            <div className="w-56 bg-indigo-300 h-screen">
-                <div>
+            <div className={` ${open ? "w-56" : "w-0"} bg-indigo-300 h-screen relative duration-300`}>
+                <img
+                    src="../../public/control.png"
+                    className={`absolute cursor-pointer -right-3 top-9 w-7 border-dark-purple border-2 rounded-full  
+                    ${!open && "rotate-180"}`}
+                    onClick={() => setOpen(!open)}/>
+                <div className={`${open ? "" : "hidden"}`}>
                     <div className="">
                         <h3 className="text-center font-semibold text-3xl m-4">Menu</h3>
                         <h1 className="text-center font-mono text-lg bg-white m-4">{`Language: ${language}`}</h1>
